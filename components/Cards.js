411b366f19f2d30c52f9c1bd6e95e08d14b5d065
Fidal <fidal15perfect@gmail.com>
2022-06-26 15:47:39 +0530

    buy method working

---
+++ components/Cards.js
@@ -0,0 +1,41 @@
+import React, { useState, useContext, useEffect } from 'react'
+import Card from './Card'
+import { AmazonContext } from '../context/AmazonContext'
+
+
+function Cards() {
+
+    const { assets } = useContext(AmazonContext);
+
+    const item = {
+        id: 0,
+        attributes: {
+            name: "Doge",
+            price: 2,
+            src: "https://images.unsplash.com/photo-1655705273978-3c5aae296870?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80"
+        }
+    }
+
+    const styles = {
+        container: `h-full w-full flex flex-col ml-[20px] `,
+        title: `text-xl font-bold mb-[20px] mt-[30px]  ml-[30px]`,
+        cards: `flex items-center  flex-wrap gap-[80px]`,
+    }
+    console.log(" cards", assets);
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.title} ><h1 style={{ fontSize: "30px" }}>  Market </h1></div>
+            <div className={styles.cards}>
+                {
+                    assets.map((item, index) => {
+                        return <Card key={item.id} item={item.attributes} productId={item.id} />
+                    })
+                }
+
+            </div>
+        </div>
+    )
+}
+
+export default Cards
